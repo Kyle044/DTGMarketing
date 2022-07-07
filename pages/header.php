9023fd6086e619cd82699da4cba97916f0783b3d
ma-beguas <jason.beguas@datagenf.com>
2022-07-07 09:12:23 +0800

    ready

--- pages/header.php
+++ pages/header.php
@@ -101,27 +101,14 @@
 
 													<ul class="dropdown-menu">
 														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">History</a>
+															<a class="dropdown-item" href="pages/contact.php">Contact</a>
 
 														</li>
 
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Facilities</a>
-
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Management Team</a>
-
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item"
-																href="#">Accreditations/Certifications</a>
-
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Data Privacy</a>
-
-														</li>
+													
+													
+													
+														
 
 
 
