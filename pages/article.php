3b948e7ba9d51941f94919989e70922c815fba21
ma-beguas <jason.beguas@datagenf.com>
2022-07-14 13:58:28 +0800

    my Changes

--- pages/article.php
+++ pages/article.php
@@ -8,19 +8,18 @@
 <div class="ar1Container">
 
 
-<h1>Your Company deserves that edge</h1>
+<h1>Datagen signed a MOA with Protec International Institute (TESDA-Accredited Institute)</h1>
 
 
 <div class="ar1ComboPic">
     <img src="../img/gallery/8.JPG" alt="">
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa a ipsa dolorem est perspiciatis nemo 
-        ipsam modi, eum alias necessitatibus quo commodi placeat officia libero aliquam 
-        explicabo itaque minus reiciendis.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa a ipsa dolorem est perspiciatis nemo 
-        ipsam modi, eum alias necessitatibus quo commodi placeat officia libero aliquam 
-        explicabo itaque minus reiciendis.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa a ipsa dolorem est perspiciatis nemo 
-        ipsam modi, eum alias necessitatibus quo commodi placeat officia libero aliquam 
+    <p>Datagen Facilities, OPC a Business Process Outsourcing (BPO) company inked a Memorandum of Agreement with
+         Protec International Institute (TESDA-Accredited Institute) on July 4, 2022. The agreement is to provide
+          opportunities for career growth and professional development of the future leaders in BPO industry.
+           It is part of the implementation of Supervised Industry Learning (SIL) for Contact Center Services NCII.
+        Part of our advocacy in Datagen, is to promote collaborative learning by engaging the students to learn, to grow, 
+        to develop their skills, and eventually offer job opportunities. 
+
 </p>
 </div>
 
