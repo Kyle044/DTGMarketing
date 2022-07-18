c96da5e7f8ab12b339873d7eab4a3a534d964cf1
Kyle <kylegwapo04@gmail.com>
2022-07-18 15:54:46 +0800

    facility and cms update

---
+++ pages/cavite.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<?php include('headerdito.php') ?>
+<body>
+
+
+
+<div class="hero hero-edit">
+    <h1>Cavite Facility</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, odit possimus fuga fugit cupiditate deserunt quas maxime consequuntur natus mollitia dolores quae provident ipsum. Placeat veniam repudiandae repellat nisi doloribus voluptate quidem, labore illo dolorem,
+         mollitia ab asperiores beatae distinctio, cum necessitatibus tempore? Numquam deleniti vel magnam ut, cum consequuntur.</p>
+         <a href="contact.php"> Contact Us</a>
+</div>
+<section>
+    <div style="height: 600px;background-image: url(../img/Cavite.JPEG);background-position: center;background-size: cover;">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-md-6 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include('footerdito.php') ?>
+</body>
+</html>
--- pages/facilities.php
+++ pages/facilities.php
@@ -8,13 +8,49 @@
 <div class="facilityContainer">
 
 
-
+<div class="hero">
+    <h1>Our Facilities And Offices</h1>
+</div>
 <div class="twoCardContainer">
-    <div ><h1>Makati Facility</h1></div>
-    <div ><h1>Cavite Facility</h1></div>
+    <a href="makati.php"><h1>Makati Facility</h1></a>
+   <a href="cavite.php"> <h1>Cavite Facility</h1></a>
 </div>
 
+<div class="container" style="margin-top:4rem;">
+    <div class="row">
+        <div class="col-md-6 fleft">
+            <h3>Our Technology</h3>
+            <ul><li>Fully integrated computer and telephony</li>
+            <li>Flexible agent scripting</li>
+            <li>Automatic call distribution</li>
+            <li>Enhanced Interactive voice response support</li>
+            <li>In-house IT professionals on call 24/7</li>
+            <li>Database integration</li>
+            <li>Predictive dialing</li>
+            <li>Multiple simultaneous campaigns</li>
+            <li>Call reports</li>
+            <li>Call recording, monitoring coaching, mentoring</li>
+            <li>Agent text messaging</li>
+            <li>Calls are recorded, batched, and processed</li>
+            <li>Receive information by secure website download, FTP, and email</li>
+            <li>Handle data entry into your website</li>
 
+        
+        
+        
+        
+        
+        </ul>
+        </div>
+        <div class="col-md-6 fright">
+            <h3>Security & Business Continuity</h3>
+            <ul><li>Data security network and IT infrastructure</li>
+            <li>24/7 security</li>
+            <li>Co-location plans with an external disaster recovery site</li>
+            <li>Data backup process.</li>
+            <li>Power blackout process.</li></ul>
+    </div>
+</div>
 
 
 
