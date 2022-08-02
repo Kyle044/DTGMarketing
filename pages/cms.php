0ae71507454e4aff3f682ba2ad1b580250af4772
iamjhojo <jhoabrematia@gmail.com>
2022-08-02 09:35:46 +0800

    test push
    test

--- pages/cms.php
+++ pages/cms.php
@@ -38,7 +38,7 @@ header("location: ./login.php");
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Admin CMS</title>
 </head>
 
@@ -97,7 +97,7 @@ header("location: ./login.php");
 
                     </tbody>
                 </table>
-                
+                <button class="button button1"><span><i class="glyphicon glyphicon-user"></i> Add User</button>
             </div>
             <!-- <button class="btn"><i class="glyphicon glyphicon-user"></i> New User</button> -->
             <!-- <table id="userTbl" class="table table-striped table-light">
