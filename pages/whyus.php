13abf5e76297c5bbf08dfca4e1674af0ec457ef7
iamjhojo <jhoabrematia@gmail.com>
2022-08-10 14:34:29 +0800

    whyus update
    fixed grid

--- pages/whyus.php
+++ pages/whyus.php
@@ -36,10 +36,10 @@
 }
 
 .whyuscontainer {
-  display: grid;
-  max-width: 1200px;
+  /* display: grid; */
+  /* max-width: 1200px; */
   margin-top: 2rem;
-  grid-template-columns: repeat(auto-fit, 22rem);
+  /* grid-template-columns: repeat(auto-fit, 22rem); */
   align-content: center;
   gap: 10px;
   justify-content: center;
@@ -51,11 +51,11 @@
   cursor: pointer;
   border-radius: 5px;
   margin-bottom: 1rem;
+  overflow-x: hidden;
 }
 
 .whyuscontainer .whyus img {
-  width: 100%;
-  height: 100%;
+  min-width:300px;
 }
 
 .whyuscontainer .whyus .whyushover {
@@ -78,22 +78,60 @@
   transform: translateY(0rem);
   text-align: center;
 }
+.galleryContainerW {
+  margin: 5rem 8rem;
+  padding-bottom: 3rem;
+}
+.gMasterContainerW {
+  background-image: url(../img/banner_inner.jpg);
+  height: auto;
+  /* height: 115vh; */
+}
+/* .galleryContainer .gallery {
+  margin: 1rem 2rem;
+  width: 100vw;
+} */
+.galleryContainerW .galleryW {
+  --grid-layout-gap: 5px;
+  --grid-column-count: 8;
+  --grid-item--min-width: 280px;
+  --gap-count: calc(var(--grid-column-count) - 1);
+  --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
+  --grid-item--max-width: calc(
+    (100% - var(--total-gap-width)) / var(--grid-column-count)
+  );
+
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
+  );
+  grid-gap: var(--grid-layout-gap);
+  justify-content: center;
+  justify-items: center;
+}
+.galleryContainerW .galleryW img {
+  height: 200px;
+  width: auto;
+  padding: 5px;
+  transition: 250ms ease;
+}
 
 </style>
 </head>
 
 <?php include('headerdito.php') ?>
 <body>
-<div class="body">
+<!-- <div class="body">
 
-<!-- body -->      
+
 <div role="main" class="main">
 
     <div class="whyusheader">
          <img src="../img/banner_inner.jpg" alt="">
     <div class="texthed">
 
-<!-- why us? --> 
+
 <div class="whyuscontainer">
 					
 <a class="whyus"  href="">
@@ -129,29 +167,51 @@
 	</div>
 			</div>
       </div>
-			</div>
+			</div> 
+</div>-->
+<div class="gMasterContainerW">
     
-      
- 
-
-
-			
-
-    
-
-
-        
- 
-
-				
-    
-
-    
-
-
-   
+        <div class="galleryContainerW" style="transform:translateY(2rem)">
+        <div class="whyuscontainer">    
+            <div class="galleryW">
+              
+              <a class="whyus"  href="">
+                  <img src="../img/whyus1.jpg" alt="">
+                  <div class="whyushover">
+                    <h3 style="color:white;">We have stringent process in selecting & hiring employees.</h3>
+                  </div>
+              </a>
+                <a class="whyus"  href="">
+                  <img src="../img/whyus2.jpg" alt="">
+                  <div class="whyushover">
+                    <h3  style="color:white;">We provide genuine customer care at a reasonable price.</h3>
+                  </div>
+              </a>	
+                <a class="whyus"  href="">
+                  <img src="../img/whyus3.jpg" alt="">
+                  <div class="whyushover">
+                    <h3  style="color:white;">We provide quick and efficient solutions to client concerns.</h3>
+                  </div>
+              </a>	
+                <a class="whyus"  href="">
+                  <img src="../img/whyus4.jpg" alt="">
+                  <div class="whyushover">
+                    <h3  style="color:white;">We can customize our technology to accommodate all business’ back-office support.</h3>
+                  </div>
+              </a>	
+              <a class="whyus"  href="">
+                  <img src="../img/whyus5.jpg" alt="">
+                  <div class="whyushover">
+                    <h3 style="color:white;">We continuously upgrading our systems and processes to cope up with evolving market trends.</h3>
+                  </div>
+              </a>
+            </div>
+            </div>  
+        </div>
     </div>
-    <?php include('footerdito.php') ?>
+
+
+<?php include('footerdito.php') ?>
 
 </body>
 
